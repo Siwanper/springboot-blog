73d4a90377278acab601480299f68729e39ef7cd
Siwanper <15091631746@qq.com>
2018-11-16 17:21:09 +0800

    11

--- src/main/java/com/swp/springboot/BlogApplication.java
+++ src/main/java/com/swp/springboot/BlogApplication.java
@@ -3,12 +3,20 @@ package com.swp.springboot;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.swp.springboot.dao")
 @EnableScheduling
-public class BlogApplication {
+public class BlogApplication extends SpringBootServletInitializer {
+
+	// 我们需要类似于web.xml的配置方式来启动spring上下文，在Application类的同级添加一个SpringBootStartApplication类
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BlogApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
